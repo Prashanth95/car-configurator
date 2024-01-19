8a1a013d340e49b2ec620c5d59ad41704c975d89
Prashanth95 <siliveriprashanth@gmail.com>
2024-01-19 12:01:49 +0530

    Add files via upload
    Successful commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app-routing.module';
+import { withFetch } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
